e05be5db6484d2a80c742714657324edc0c2131a
PryVlad <pr.vl@outlook.com>
2024-09-04 12:31:39 +0200

    Initial Commit

---
+++ Setv2/Setv2App.swift
@@ -0,0 +1,17 @@
+//
+//  Setv2App.swift
+//  Setv2
+//
+//  Created by Vladyslav Pryl on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Setv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
