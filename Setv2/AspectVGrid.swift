f304bc588ea57306a760253bbf81d169c007f1b6
PryVlad <pr.vl@outlook.com>
2024-09-07 19:24:22 +0200

    Lecture6

---
+++ Setv2/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Setv2
+//
+//  Created by Vladyslav Pryl on 05.09.2024.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
---
+++ Setv2/Wall.swift
@@ -0,0 +1,8 @@
+//
+//  Wall.swift
+//  Setv2
+//
+//  Created by Vladyslav Pryl on 05.09.2024.
+//
+
+import Foundation
